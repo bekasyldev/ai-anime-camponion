fa8f8427e5ccf69a29edff2252e09958ab12ecd8
Bekasyl <devbekasyl@gmail.com>
2024-02-18 16:32:53 +0600

    ui-components

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import SearchInput from "@/components/search-input";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="w-full p-4 space-y-2">
+      <SearchInput />
+      <div>Root Page</div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ components/navbar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Menu, Sparkles } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./theme-toggle";
+import MobileSidebar from "./mobile-sidebar";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
+      <div className="flex items-center">
+        <MobileSidebar />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block font-semibold text-xl md:text-3xl text-primary",
+              font.className
+            )}
+          >
+            anime.<span className="text-purple-700">ai</span>
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button size={"sm"} variant={"premium"}>
+          Updgrade
+          <Sparkles className="w-4 h-4 fill-white text-white ml-2" />
+        </Button>
+        <ThemeToggle />
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
